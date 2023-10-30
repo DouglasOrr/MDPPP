0b34f80c3200fb16b8a356b53bda3170b6e4c4f6
Douglas Orr <douglas.orr99@gmail.com>
2023-10-30 07:59:12 +0000

    Put some objects on the screen

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
 import * as Phaser from "phaser"
 
-export default class Demo extends Phaser.Scene {
+const S = {
+  paddlew: 0.2,
+}
+
+const WHITE = 0xffffffff
+
+export default class Game extends Phaser.Scene {
+  ball: Phaser.GameObjects.Arc | null
+  paddles: Phaser.GameObjects.Rectangle[]
+
   constructor() {
-    super("demo")
+    super("game")
+    this.ball = null
+    this.paddles = []
   }
 
   preload(): void {}
 
   create(): void {
-    this.add.rectangle(100, 100, 50, 100, 0xff00ffff)
+    this.ball = this.add.circle(0, 0, 0.02, WHITE)
+    this.paddles = [
+      this.add.rectangle(0, -1, S.paddlew, 0.02, WHITE),
+      this.add.rectangle(0, 1, S.paddlew, 0.02, WHITE),
+    ]
+    const camera = this.cameras.main
+    camera.setZoom(camera.width, camera.height / 2)
+    camera.setScroll(-camera.width / 2, -camera.height / 2)
   }
 }
 
 export const game = new Phaser.Game({
   type: Phaser.AUTO,
   backgroundColor: "#000000",
-  width: 800,
-  height: 600,
-  scene: Demo,
+  width: 400,
+  height: 800,
+  scene: Game,
 })
