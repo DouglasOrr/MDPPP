2183b8f345982f5dc17480a8efc49bee157527f8
Douglas Orr <douglas.orr99@gmail.com>
2023-11-05 05:31:38 +0000

    Add a barebones NdArray component

---
+++ src/ndarray.ts
@@ -0,0 +1,302 @@
+// Array utilities
+
+export function arrayEquals<T>(a: T[], b: T[]): boolean {
+  return a.length === b.length && a.every((value, i) => value === b[i])
+}
+
+export function arrayProduct(array: number[]): number {
+  return array.reduce((a, b) => a * b, 1)
+}
+
+// Assertions
+
+export type Message = string | (() => string)
+
+export function messageString(message: Message): string {
+  return typeof message === "function" ? message() : message
+}
+
+export function assert(condition: boolean, message: Message): void {
+  if (!condition) {
+    throw Error(messageString(message))
+  }
+}
+
+export function assertEquals(a: any, b: any, message: Message = ""): void {
+  assert(a === b, () => `Expected ${a} === ${b};  ${messageString(message)}`)
+}
+
+export function assertArrayEquals<T>(
+  a: T[],
+  b: T[],
+  message: Message = "",
+): void {
+  assert(
+    arrayEquals(a, b),
+    () =>
+      `Expected arrayEquals([${a.toString()}], [${b.toString()}])` +
+      `;  ${messageString(message)}`,
+  )
+}
+
+// NdArray
+
+// C-Contiguous multidimensional array
+export class NdArray {
+  shape: number[]
+  data: number[]
+
+  constructor(shape: number[], data: number[] | null = null) {
+    this.shape = shape
+    const nelement = arrayProduct(shape)
+    if (data === null) {
+      this.data = Array(nelement)
+    } else {
+      assertEquals(
+        data.length,
+        nelement,
+        "NdArray shape (nelement) != data.length",
+      )
+      this.data = data
+    }
+  }
+
+  // Basics
+
+  ndim(): number {
+    return this.shape.length
+  }
+
+  strides(): number[] {
+    const result = Array(this.shape.length)
+    let stride = 1
+    for (let i = result.length - 1; i !== -1; --i) {
+      result[i] = stride
+      stride *= this.shape[i]
+    }
+    return result
+  }
+
+  clone(): NdArray {
+    return new NdArray(this.shape, [...this.data])
+  }
+
+  // Initialisation
+
+  fill_(value: number): NdArray {
+    this.data.fill(value)
+    return this
+  }
+
+  rand_(low: number = 0, high: number = 1): NdArray {
+    return this.map_(() => Math.random() * (high - low) + low)
+  }
+
+  // Transformations
+
+  map_(fn: (x: number, i: number) => number): NdArray {
+    for (let i = 0; i < this.data.length; ++i) {
+      this.data[i] = fn(this.data[i], i)
+    }
+    return this
+  }
+
+  view_(shape: number[]): NdArray {
+    assertEquals(
+      arrayProduct(shape),
+      this.data.length,
+      "view shape should retain same nelement",
+    )
+    this.shape = shape
+    return this
+  }
+
+  t(): NdArray {
+    const s = this.shape
+    const thisSize1 = s[s.length - 1]
+    const thisSize2 = s[s.length - 2]
+    const groupSize = thisSize1 * thisSize2
+    const result = new NdArray([...s.slice(0, -2), thisSize1, thisSize2])
+    this.data.forEach((value, i) => {
+      const di =
+        Math.floor(i / groupSize) * groupSize +
+        (i % thisSize1) * thisSize2 +
+        Math.floor((i / thisSize1) % thisSize2)
+      result.data[di] = value
+    })
+    return result
+  }
+
+  // Dims maps result dimension to this dimension
+  transpose(dims: number[]): NdArray {
+    assertArrayEquals(
+      dims.slice().sort((a, b) => a - b),
+      [...Array(this.ndim()).keys()],
+      "transpose dims should cover [0..(ndim-1)]",
+    )
+    const result = new NdArray(dims.map((i) => this.shape[i]))
+    const thisStrides = this.strides()
+    const resultStrides = result.strides()
+    const thisData = this.data
+    function copy(dim: number, resultI: number, thisI: number): void {
+      const resultS = resultStrides[dim]
+      const thisS = thisStrides[dims[dim]]
+      if (dim === result.ndim() - 1) {
+        for (let i = 0; i < result.shape[dim]; ++i) {
+          result.data[resultI + resultS * i] = thisData[thisI + thisS * i]
+        }
+      } else {
+        for (let i = 0; i < result.shape[dim]; ++i) {
+          copy(dim + 1, resultI + resultS * i, thisI + thisS * i)
+        }
+      }
+    }
+    copy(0, 0, 0)
+    return result
+  }
+
+  // Dims maps this dimension to destination dimension
+  untranspose(dims: number[]): NdArray {
+    const transposeDims = Array(dims.length)
+    dims.forEach((dim, i) => (transposeDims[dim] = i))
+    return this.transpose(transposeDims)
+  }
+
+  add_(rhs: NdArray): NdArray {
+    assertArrayEquals(this.shape, rhs.shape, "add this.shape === rhs.shape")
+    for (let i = 0; i < this.data.length; ++i) {
+      this.data[i] += rhs.data[i]
+    }
+    return this
+  }
+
+  // Always over the final dimension
+  logSoftmax_(): NdArray {
+    const stride = this.shape[this.shape.length - 1]
+    for (let j = 0; j < this.data.length; j += stride) {
+      const slice = this.data.slice(j, j + stride)
+      const max = slice.reduce((a, b) => Math.max(a, b), -Infinity)
+      const logSumExp = Math.log(
+        slice.reduce((sum, x) => sum + Math.exp(x - max), 0),
+      )
+      for (let i = j; i < j + stride; ++i) {
+        this.data[i] -= max + logSumExp
+      }
+    }
+    return this
+  }
+
+  // (*g, m, k) @ (*g, k, n) -> (*g, m, n)
+  dot(rhs: NdArray): NdArray {
+    assertArrayEquals(
+      this.shape.slice(0, -2),
+      rhs.shape.slice(0, -2),
+      "dot - group dimensions should match",
+    )
+    const ndim = this.ndim()
+    assertEquals(
+      this.shape[ndim - 1],
+      rhs.shape[ndim - 2],
+      "dot - concat dimension should match (..., :, ?).dot((..., ?, :))",
+    )
+    const m = this.shape[ndim - 2]
+    const k = this.shape[ndim - 1]
+    const n = rhs.shape[ndim - 1]
+    const result = new NdArray([...this.shape.slice(0, -2), m, n])
+    // Naive matmul
+    for (let i = 0; i < result.data.length; ++i) {
+      let sum = 0.0
+      const thisi = Math.floor(i / n) * k
+      const rhsi = Math.floor(i / (m * n)) * k * n + (i % n)
+      for (let j = 0; j < k; ++j) {
+        sum += this.data[thisi + j] * rhs.data[rhsi + n * j]
+      }
+      result.data[i] = sum
+    }
+    return result
+  }
+
+  // Gather/scatter
+
+  // Grouped on leading dimensions of indices, broadcast on trailing dimensions of this
+  // E.g. {shape: [2, 3, 4]}.gather({shape: [2, 5]}) => {shape: [2, 5, 4]}
+  gather(indices: NdArray): NdArray {
+    assertArrayEquals(
+      indices.shape.slice(0, -1),
+      this.shape.slice(0, indices.ndim() - 1),
+      "gather - leading dimensions of indices should match this.shape",
+    )
+    const rowShape = this.shape.slice(indices.ndim())
+    const rowSize = arrayProduct(rowShape)
+    const gatherSize = indices.shape[indices.ndim() - 1]
+    const tableSize = this.shape[indices.ndim() - 1]
+    const result = new NdArray(indices.shape.concat(rowShape))
+    indices.data.forEach((index, i) => {
+      const di =
+        Math.floor(i / gatherSize) * tableSize * rowSize + index * rowSize
+      result.data.splice(
+        i * rowSize,
+        rowSize,
+        ...this.data.slice(di, di + rowSize),
+      )
+    })
+    return result
+  }
+
+  // Grouped on leading dimensions of indices, broadcast on trailing dimensions of this
+  // E.g. {shape: [2, 5, 4]}.scatter({shape: [2, 5]}, 3) => {shape: [2, 3, 4]}
+  scatter(indices: NdArray, size: number): NdArray {
+    assertArrayEquals(
+      indices.shape,
+      this.shape.slice(0, indices.ndim()),
+      "scatter - indices shape should match this.shape",
+    )
+    const rowShape = this.shape.slice(indices.ndim())
+    const rowSize = arrayProduct(rowShape)
+    const tableSize = indices.shape[indices.ndim() - 1]
+    const result = new NdArray([
+      ...indices.shape.slice(0, -1),
+      size,
+      ...rowShape,
+    ]).fill_(0)
+    indices.data.forEach((index, i) => {
+      const ri = Math.floor(i / tableSize) * size * rowSize + index * rowSize
+      for (let j = 0; j < rowSize; ++j) {
+        result.data[ri + j] += this.data[i * rowSize + j]
+      }
+    })
+    return result
+  }
+
+  // Grouped on leading dimensions of indices, broadcast on trailing dimensions of src
+  // E.g. {shape: [2, 3, 4]}.scatter_write({shape: [2, 5, 4]}, {shape: [2, 5]})
+  scatterWrite_(src: NdArray, indices: NdArray): NdArray {
+    assertArrayEquals(
+      src.shape.slice(0, indices.ndim()),
+      indices.shape,
+      "scatterWrite_ - leading dimensions of src should match indices.shape",
+    )
+    assertArrayEquals(
+      indices.shape.slice(0, -1),
+      this.shape.slice(0, indices.ndim() - 1),
+      "scatterWrite_ - indices shape should match this.shape",
+    )
+    const rowShape = src.shape.slice(indices.ndim())
+    assertArrayEquals(
+      this.shape.slice(indices.ndim()),
+      rowShape,
+      "scatterWrite_ - trailing dimensions of src.shape should match this.shape",
+    )
+    const rowSize = arrayProduct(rowShape)
+    const tableSize = indices.shape[indices.ndim() - 1]
+    const scatterSize = this.shape[indices.ndim() - 1]
+    indices.data.forEach((index, i) => {
+      const ri =
+        Math.floor(i / tableSize) * scatterSize * rowSize + index * rowSize
+      for (let j = 0; j < rowSize; ++j) {
+        this.data[ri + j] = src.data[i * rowSize + j]
+      }
+    })
+    return this
+  }
+}
---
+++ tests/ndarray.test.ts
@@ -0,0 +1,116 @@
+import { NdArray } from "../src/ndarray"
+
+test("new NdArray", () => {
+  const a = new NdArray([1, 2, 3]).fill_(100)
+  expect(a.shape).toStrictEqual([1, 2, 3])
+  expect(a.data).toStrictEqual([100, 100, 100, 100, 100, 100])
+  expect(a.ndim()).toBe(3)
+  expect(a.strides()).toStrictEqual([6, 3, 1])
+
+  const b = new NdArray([2, 1], [1000, 2000])
+  expect(b.shape).toStrictEqual([2, 1])
+  expect(b.data).toStrictEqual([1000, 2000])
+
+  expect(() => new NdArray([2, 1], [1000, 2000, 3000])).toThrow("3 === 2")
+})
+
+test("clone, map_", () => {
+  const a = new NdArray([1, 3]).fill_(100)
+  const b = a.clone().map_((x, i) => 2 * x + i)
+  expect(a.data).toStrictEqual([100, 100, 100])
+  expect(b.data).toStrictEqual([200, 201, 202])
+})
+
+test("rand_", () => {
+  const a = new NdArray([1, 20]).rand_(100, 200)
+  expect(a.data.map((x) => 100 <= x && x <= 200)).toStrictEqual(
+    Array(20).fill(true),
+  )
+})
+
+test("view, t, transpose, untranspose", () => {
+  const flatA = new NdArray([12]).map_((_, i) => i)
+  const a = flatA.clone().view_([2, 1, 3, 2])
+  expect(a.shape).toStrictEqual([2, 1, 3, 2])
+  expect(a.data).toStrictEqual(flatA.data)
+
+  let aT = a.t()
+  expect(aT.shape).toStrictEqual([2, 1, 2, 3])
+  const transposedDataA = [0, 2, 4, 1, 3, 5, 6, 8, 10, 7, 9, 11]
+  expect(aT.data).toStrictEqual(transposedDataA)
+
+  aT = a.transpose([0, 1, 3, 2])
+  expect(aT.shape).toStrictEqual([2, 1, 2, 3])
+  expect(aT.data).toStrictEqual(transposedDataA)
+  expect(aT.untranspose([0, 1, 3, 2])).toMatchObject(a)
+
+  const aT2 = a.transpose([3, 2, 0, 1])
+  expect(aT2.shape).toStrictEqual([2, 3, 2, 1])
+  expect(aT2.data).toStrictEqual([0, 6, 2, 8, 4, 10, 1, 7, 3, 9, 5, 11])
+  expect(aT2.untranspose([3, 2, 0, 1])).toMatchObject(a)
+})
+
+test("add_", () => {
+  expect(
+    new NdArray([2, 3])
+      .map_((_, i) => 100 + 2 * i)
+      .add_(new NdArray([2, 3]).map_((_, i) => i)),
+  ).toMatchObject(new NdArray([2, 3], [100, 103, 106, 109, 112, 115]))
+
+  expect(() => new NdArray([2, 3]).add_(new NdArray([6]))).toThrow(
+    "arrayEquals([2,3], [6])",
+  )
+})
+
+test("logSoftmax_", () => {
+  const a = new NdArray(
+    [2, 1, 3],
+    [1000, 1000, 1000, Math.log(1 / 2), 0, Math.log(1 / 2)],
+  )
+  const expected = [1 / 3, 1 / 3, 1 / 3, 0.25, 0.5, 0.25]
+
+  const sa = a.clone().logSoftmax_().map_(Math.exp)
+  expect(sa.shape).toStrictEqual([2, 1, 3])
+  sa.data.forEach((v, i) => {
+    expect(v).toBeCloseTo(expected[i], 4)
+  })
+})
+
+test("dot", () => {
+  const a0 = [-1, 0, 2, 0]
+  const a1 = [0, 0, 0, 2]
+  const a2 = [0, 0, 0, 0]
+  const a = new NdArray([2, 3, 4], [...a0, ...a1, ...a2, ...a0, ...a1, ...a2])
+  const b = new NdArray([2, 4, 5]).map_((_, i) => i)
+
+  const c = a.dot(b)
+  expect(c.shape).toStrictEqual([2, 3, 5])
+  const c00 = [20, 21, 22, 23, 24]
+  const c01 = [30, 32, 34, 36, 38]
+  const c02 = [0, 0, 0, 0, 0]
+  const c10 = [40, 41, 42, 43, 44]
+  const c11 = [70, 72, 74, 76, 78]
+  const c12 = [0, 0, 0, 0, 0]
+  expect(c.data).toStrictEqual([...c00, ...c01, ...c02, ...c10, ...c11, ...c12])
+
+  expect(
+    new NdArray([5, 4, 3]).fill_(11).dot(new NdArray([5, 3, 7]).fill_(2)),
+  ).toMatchObject(new NdArray([5, 4, 7]).fill_(66))
+})
+
+test("gather, scatter, scatterWrite_", () => {
+  const table = new NdArray([2, 5, 2]).map_((_, i) => i)
+  expect(table.gather(new NdArray([2, 3], [4, 0, 4, 4, 1, 1]))).toMatchObject(
+    new NdArray([2, 3, 2], [8, 9, 0, 1, 8, 9, 18, 19, 12, 13, 12, 13]),
+  )
+
+  const idx = new NdArray([2, 5], [1, 2, 0, 0, 0, 1, 1, 1, 1, 1])
+  expect(table.scatter(idx, 3)).toMatchObject(
+    new NdArray([2, 3, 2], [18, 21, 0, 1, 2, 3, 0, 0, 70, 75, 0, 0]),
+  )
+  expect(
+    new NdArray([2, 3, 2]).fill_(-1).scatterWrite_(table, idx),
+  ).toMatchObject(
+    new NdArray([2, 3, 2], [8, 9, 0, 1, 2, 3, -1, -1, 18, 19, -1, -1]),
+  )
+})
